03beb40c0353247d9315c5a597608cc585b0ffa6
Ohayoo <jan.kardas@seznam.cz>
2017-10-18 12:02:02 +0200

    ImgGeo Debug

--- cctv/imggeo.go
+++ cctv/imggeo.go
@@ -11,15 +11,15 @@ import (
 func Imggeo(m *email.Message, sms *structures.SMSMessage) {
 	for _, part := range m.MessagesAll() {
 		mediaType, params, err := part.Header.ContentType()
-		if err != nil {
+		if err == nil {
 			log.Println("MediaType: " + mediaType)
 			log.Println("Body: " + string(part.Body))
 			log.Println(part)
+			for _, param := range params {
+				log.Println("Param: " + param)
+			}
 		} else {
 			log.Println(err)
-		}
-		for _, param := range params {
-			log.Println("Param: " + param)
 		}
 	}
 }
